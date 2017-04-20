656d7a35e07e4cb6b0cf1712e472c416e2f216ee
LucianCoso <cosofret.lucian@gmail.com>
2017-04-20 22:40:00 +0300

    Add files via upload

--- R/readdocx.R
+++ R/readdocx.R
@@ -1,4 +1,37 @@
 
+getTablesFromDocx <- function(word_doc)
+  {
+  library("xml2")
+
+  tmpd <- tempdir()
+  tmpf <- tempfile(tmpdir=tmpd, fileext=".zip")
+
+  file.copy(word_doc, tmpf)
+  unzip(tmpf, exdir=sprintf("%s/docdata", tmpd))
+
+  doc <- read_xml(sprintf("%s/docdata/word/document.xml", tmpd))
+
+  unlink(tmpf)
+  unlink(sprintf("%s/docdata", tmpd), recursive=TRUE)
+
+  ns <- xml_ns(doc)
+
+  tbls <- xml_find_all(doc, ".//w:tbl", ns=ns)
+
+  lapply(tbls, function(tbl)
+    {
+    cells <- xml_find_all(tbl, "./w:tr/w:tc", ns=ns)
+    rows <- xml_find_all(tbl, "./w:tr", ns=ns)
+    dat <- data.frame(matrix(xml_text(cells),
+                             ncol=(length(cells)/length(rows)),
+                             byrow=TRUE),
+                             stringsAsFactors=FALSE)
+    colnames(dat) <- dat[1,]
+    dat <- dat[-1,]
+    rownames(dat) <- NULL
+    dat
+    })
+  }
 
 # Read docx file
 readDoc <- function(file)
