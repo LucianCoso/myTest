45f9378427ded023d3d6567bbb34297f796b949a
LucianCoso <cosofret.lucian@gmail.com>
2017-04-22 17:03:49 +0300

    Add files via upload

--- R/onLoad.R
+++ R/onLoad.R
@@ -7,6 +7,5 @@
   utils::data(countries_data, package = pkg, envir = parent.env(environment()))
   utils::data(transaction_data, package = pkg, envir = parent.env(environment()))
   utils::data(transaction_rules, package = pkg, envir = parent.env(environment()))
-  utils::data(normalPacient, package = pkg, envir = parent.env(environment()))
 }
 
