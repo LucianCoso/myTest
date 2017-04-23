a2a125e00f5ed8be49036e6f14e4bd0b79252316
LucianCoso <cosofret.lucian@gmail.com>
2017-04-23 12:51:14 +0300

    Add files via upload

--- R/asociationRule.R
+++ R/asociationRule.R
@@ -8,16 +8,13 @@ getRecommendations <- function(input)
   item2 <- toString(items[2])
   item3 <- toString(items[3])
 
-  #subset_rule <- apriori(transaction_data, parameter = list(support = 0.006, confidence = 0.25, minlen = 2, target="rules"), appearance = list(lhs = c(item1, item2), default="rhs"))
-  subset_rule <- subset(transaction_rules, lhs %in% item1)
-
-  #if(!is.na(item1) && !is.na(item2) && !is.na(item3)){
-  #  subset_rule <- subset(transaction_rules, lhs %ain% c(item1, item2, item3))
-  #} else if(!is.na(item1) && !is.na(item2)){
-  #  subset_rule <- subset(transaction_rules, lhs %ain% c(item1, item2))
-  #} else if(!is.na(item1)) {
-  #  subset_rule <- subset(transaction_rules, lhs %in% c(item1))
-  #}
+  if(!is.na(item1) && !is.na(item2) && !is.na(item3)){
+    subset_rule <- subset(transaction_rules, lhs %ain% c(item1, item2, item3))
+  } else if(!is.na(item1) && !is.na(item2)){
+    subset_rule <- subset(transaction_rules, lhs %ain% c(item1, item2))
+  } else if(!is.na(item1)) {
+    subset_rule <- subset(transaction_rules, lhs %in% c(item1))
+  }
 
   #if(length(subset_rule) == 0)
   #{
